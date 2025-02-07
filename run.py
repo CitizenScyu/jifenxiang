fe574de947995776b47c382a677dafc019789395
CitizenScyu <58416773+CitizenScyu@users.noreply.github.com>
2025-02-07 11:32:21 +0800

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from bot.bot import PointsBot
-from config import BOT_TOKEN, WEBDAV_CONFIG, DATABASE_FILE
-
-def main():
-    bot = PointsBot(BOT_TOKEN, WEBDAV_CONFIG, DATABASE_FILE)
-    bot.run()
-
-if __name__ == '__main__':
-    main()
